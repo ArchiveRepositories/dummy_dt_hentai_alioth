92e1b951c257d336b490922da76612b04124e3ee
johnmart19 <johnivan19999@gmail.com>
2021-10-15 20:47:42 +0300

    hentai_alioth-SaltyIceCream-ota-sp1a.210812.016-Furry.10101602

--- lineage_alioth.mk
+++ lineage_alioth.mk
@@ -17,7 +17,7 @@ PRODUCT_MODEL := K40
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR := xiaomi
 TARGET_VENDOR_PRODUCT_NAME := alioth
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="hentai_alioth-user 12 SP1A.210812.016 20211008 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="hentai_alioth-user 12 SP1A.210812.016 20211009 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := Redmi/hentai_alioth/alioth:12/SP1A.210812.016/20211008:user/release-keys
+BUILD_FINGERPRINT := Redmi/hentai_alioth/alioth:12/SP1A.210812.016/20211009:user/release-keys
